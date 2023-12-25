0fc9f02536dc73aa8327732133fd427202cd110d
Alex <alexdev0605@gmail.com>
2023-12-25 22:10:36 +0100

    feat: add use debounce package

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,12 +1,33 @@
+"use client";
 import styles from "./search.module.css";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Search as SearchIcon } from "lucide-react";
 
 const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = (e) => {
+    const params = new URLSearchParams(searchParams);
+    if (e.target.value) {
+      e.target.value.length > 2 && params.set("q", e.target.value);
+    } else {
+      params.delete("q");
+    }
+    replace(`${pathname}?${params}`);
+  };
+
   return (
     <div className={styles.container}>
       <SearchIcon />
-      <input type="text" className={styles.input} placeholder={placeholder} />
+      <input
+        type="text"
+        className={styles.input}
+        placeholder={placeholder}
+        onChange={handleSearch}
+      />
     </div>
   );
 };
