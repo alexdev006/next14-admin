12a0c4829701e0222b3eea9a1dc87c35907a88b6
Alex <alexdev0605@gmail.com>
2023-12-25 22:17:29 +0100

    feat: add use debounce to search component

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -3,13 +3,15 @@ import styles from "./search.module.css";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Search as SearchIcon } from "lucide-react";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }) => {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
-  const handleSearch = (e) => {
+  //le debounce permet de trigger la recherche uniquement 300ms après la fin de la saisie
+  const handleSearch = useDebouncedCallback((e) => {
     const params = new URLSearchParams(searchParams);
     if (e.target.value) {
       e.target.value.length > 2 && params.set("q", e.target.value);
@@ -17,7 +19,7 @@ const Search = ({ placeholder }) => {
       params.delete("q");
     }
     replace(`${pathname}?${params}`);
-  };
+  }, 300);
 
   return (
     <div className={styles.container}>
